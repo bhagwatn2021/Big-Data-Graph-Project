bda849539e3a4a0601b1e7eb4c8d08b2bd2deef4
Fleurevca Francois <fleurevca.francois@bison.howard.edu>
2020-02-09 14:20:05 -0500

    Installed igraph and sna packages inton the script

--- project1master.R
+++ project1master.R
@@ -1,6 +1,10 @@
 # Master File for Class Project 1
 # Fleurevca Francois, Andrew Nguyen, Neel Bhagwat, Eptisam Kassim
 
+# Question 2
+install.packages("igraph")
+install.packages("sna")
+
 
 # Question 3
 

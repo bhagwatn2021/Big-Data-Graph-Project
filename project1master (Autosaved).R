455fbc2d7bd00af5c46980eb3ad75ebd8a21bf06
Neel <neil.bhagwat1999@gmail.com>
2020-02-12 14:12:45 -0500

    hello

--- project1master (Autosaved).R
+++ project1master (Autosaved).R
@@ -9,7 +9,8 @@ library(igraph)
 # creates umsimplified graph and plots it
 el <- read.table("data/email-EU/email-EU.edges")
 unSimGraph <- graph.data.frame(el, directed = FALSE)
-plot(unSimGraph)
+
+#plot(unSimGraph)
 
 
 # Question 3

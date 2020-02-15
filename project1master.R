f959bf9d6a29a9a9cdb80f9666821bc679f13321
Neel <neil.bhagwat1999@gmail.com>
2020-02-15 15:12:18 -0500

    Compute longest paths

--- project1master.R
+++ project1master.R
@@ -8,9 +8,8 @@ library(igraph)
 
 # creates umsimplified graph and plots it
 el <- read.table("data/email-EU/email-EU.edges")
-unSimGraph <- graph.data.frame(el, directed = FALSE)
+unSimGraph <- graph.data.frame(el, directed = TRUE)
 plot(unSimGraph)
-
 
 # Question 3
 
@@ -18,6 +17,14 @@ plot(unSimGraph)
 # Question 4
 
 
-
 # Question 5
+#Longest path in the graph
+get.diameter (unSimGraph, directed = TRUE, unconnected = TRUE)
+farthest.nodes(unSimGraph, directed = TRUE, unconnected = TRUE)
+
+#Delete longest path and try longest path functions again to determine multiple longest path
+multLong <- delete_edges(unSimGraph, c("999|938"))
+
+get.diameter (multLong, directed = TRUE, unconnected = TRUE)
+farthest.nodes(multLong, directed = TRUE, unconnected = TRUE)
 

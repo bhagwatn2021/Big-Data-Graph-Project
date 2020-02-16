59cb757d141e60bc5bafb6913202fa36fb7906dc
Fleurevca Francois <fleurevca.francois@bison.howard.edu>
2020-02-16 16:36:26 -0500

    some simple changes

--- project1master.R
+++ project1master.R
@@ -2,14 +2,13 @@
 # Fleurevca Francois, Andrew Nguyen, Neel Bhagwat, Eptisam Kassim
 
 # Base for each Question/Question 2
-install.packages("igraph")
-install.packages("sna")
+
 library(igraph)
 
 # creates umsimplified graph and plots it
 el <- read.table("data/email-EU/email-EU.edges")
 unSimGraph <- graph.data.frame(el, directed = FALSE)
-plot(unSimGraph)
+#plot(unSimGraph)
 
 
 # Question 3

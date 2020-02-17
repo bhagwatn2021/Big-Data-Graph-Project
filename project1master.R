1f24edf4bb79b151b6c253c9fa74a3a64600fb4c
Neel Bhagwat <neil.bhagwat1999@gmail.com>
2020-02-17 18:35:05 -0500

    Import libraries into R

--- project1master.R
+++ project1master.R
@@ -1,9 +1,15 @@
 # Master File for Class Project 1
 # Fleurevca Francois, Andrew Nguyen, Neel Bhagwat, Eptisam Kassim
 
-# Base for each Question/Question 2
-
+# Base for each Question
+install.packages("igraph")
+install.packages("sna")
 library(igraph)
+
+# creates unsimplified graph and plots it
+el <- read.table("data/email-EU/email-EU.edges")
+unSimGraph <- graph.data.frame(el, directed = TRUE)
+plot(unSimGraph)
 
 # creates umsimplified graph and plots it
 el <- read.table("data/email-EU/email-EU.edges")

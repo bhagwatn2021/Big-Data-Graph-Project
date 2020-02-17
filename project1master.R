6925d8a13987e39af48e465988aebb9427c5daa2
Eptisam Kassim <eptisam@Eptisams-MacBook-Pro.local>
2020-02-17 17:56:43 -0500

    q3 new functions

--- project1master.R
+++ project1master.R
@@ -9,14 +9,23 @@ library(igraph)
 # creates umsimplified graph and plots it
 el <- read.table("data/email-EU/email-EU.edges")
 unSimGraph <- graph.data.frame(el, directed = FALSE)
-#plot(unSimGraph)
+plot(unSimGraph)
 
 
 # Question 3
+edge_density(unSimGraph, loops = FALSE)
+diameter(unSimGraph, directed = TRUE, unconnected = TRUE, weights = NULL)
+edge_betweenness(unSimGraph, e = E(unSimGraph), directed = TRUE, weights = NULL)
+mean_distance(unSimGraph, directed = TRUE, unconnected = TRUE)
 
 
 # Question 4
-
-
+gsize(unSimGraph)
+count_triangles(unSimGraph, vids = V(unSimGraph))
+bipartite_mapping(unSimGraph)
+assortativity_degree(unSimGraph, directed = TRUE)
+components(unSimGraph, mode = c("weak", "strong"))
+hub_score(unSimGraph, scale = TRUE, weights = NULL, options = arpack_defaults)
 
 # Question 5
+
